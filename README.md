d913f114abd2c50ca3c74293a6fd8020859eaa9d
lamorasjr <lamorasjr@gmail.com>
2024-06-14 20:52:08 +0200

    completed the project 1s phase

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# Load to Postgres-Supabase
+# Load to PostgreSQL-Supabase
+
+## Description
+Python project to read a sample csv file and load into a PostgresSQL database.
+
+
+## Requirements
+**1. Clone this repository**
+```cmd
+git clone https://github.com/lamorasjr/load_to_postgres.git
+```
+
+**2. Create a new .venv for this project (Optional)**
+
+
+**3. Install the required Python dependencies**
+```cmd
+pip install -r src/requirements.txt
+```
+
+**4. Create '.env' and fill the variables as in the 'env-template'**
+
+
+## References
+* Sample dataset from: https://www.kaggle.com/datasets/zeesolver/spotfy
--- src/extract_load.py
+++ src/extract_load.py
@@ -3,12 +3,29 @@ from sqlalchemy import create_engine
 from dotenv import load_dotenv
 import os
 
+load_dotenv()
+
 # Get enviroment variables
-DB_USER =
-DB_PASSWORD =
-DB_HOST = 
-DB_PORT =
-DB_NAME = 
+DB_HOST = os.getenv('DB_HOST')
+DB_PORT = os.getenv('DB_PORT')
+DB_NAME = os.getenv('DB_NAME')
+DB_USER = os.getenv('DB_USER')
+DB_PASS = os.getenv('DB_PASS')
+DB_SCHEMA = os.getenv('DB_SCHEMA')
 
+database_url = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
-engine = create_engine(f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}")
+engine = create_engine(database_url)
+
+def convert_csv_to_df(csv_name:str):
+    df = pd.read_csv(csv_name, encoding="ISO-8859-1")
+    df.rename(columns={"artist(s)_name" : "artist_name"}, inplace=True)
+    return df
+
+def load_to_postgres(df, db_schema="public"):
+    df.to_sql("kaggle_popular_spotify_songs", engine, if_exists="replace", index=False, schema=db_schema)
+    print(f"The dataset has been loaded into the '{db_schema}' / Supabase-PostgreSQL database.")
+
+if __name__ == "__main__":
+    df = convert_csv_to_df("data\popular_spotify_songs.csv")
+    load_to_postgres(df, db_schema=DB_SCHEMA)
